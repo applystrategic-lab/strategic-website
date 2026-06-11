622bf3084ae749f703d28d393ab4148584763bc5
Strategic <applystrategic@gmail.com>
2026-06-11 14:44:16 +0800

    Updated professional steam section in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -232,7 +232,7 @@ export default function AboutPage() {
                   </h3>
 
                   <p className="text-[#D4A017] uppercase tracking-[3px] text-sm">
-                    Operations Manager
+                    Operations
                   </p>
 
                 </div>
